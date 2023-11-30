cd37b4060b95776f1e7f0a5bf4393a5867f9b3a4
PUCHA SRINIVASA PAVAN <82796461+srinivas50505@users.noreply.github.com>
2023-11-30 15:33:50 +0530

    Add files via upload

---
+++ EXP1.py
@@ -0,0 +1,6 @@
+import csv
+with open(r'C:\Users\acer\Desktop\Srinivas-09\departments.csv', newline='') as csvfile:
+ data = csv.reader(csvfile, delimiter=',', quotechar='|')
+ for row in data:
+     for value in row:
+         print(value) 
