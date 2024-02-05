3e165929e137ff259cd49a80975c10e34904bfc6
Srinivasa Pavan <82796461+srinivas50505@users.noreply.github.com>
2024-02-05 21:13:31 +0530

    Update and rename EXP1.py to EXP1(EXTRACT ROW).py

--- EXP1(EXTRACT ROW).py
+++ EXP1(EXTRACT ROW).py
@@ -3,4 +3,5 @@ with open(r'C:\Users\acer\Desktop\Srinivas-09\departments.csv', newline='') as c
  data = csv.reader(csvfile, delimiter=',', quotechar='|')
  for row in data:
      for value in row:
-         print(value) 
+         print(value) 
+
